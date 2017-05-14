22dadda64203121a61c3f9c64da998807a641b13
khthana <khthana@hotmail.com>
2017-05-14 10:25:09 +0700

    Create 3,0_8266_Switch_Interrupt.ino

---
+++ 3,0_8266_Switch_Interrupt.ino
@@ -0,0 +1,23 @@
+#define D0 16 // USER LED Wake
+#define D1 0 // FLASH
+#define buttonPin1 D1 // the number of the pushbutton pin
+#define ledPin D0 // the number of the LED pin
+
+void setup()
+{
+  Serial.begin(9600); // initialize serial:
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin1,INPUT_PULLUP);
+  attachInterrupt(buttonPin1,EXTI3_ISR,CHANGE);
+}
+
+void loop()
+{
+  delay(250);
+}
+
+void EXTI3_ISR()
+{
+  digitalWrite(ledPin,!digitalRead(ledPin));
+  Serial.println("Interrupt from pin D1");
+}
