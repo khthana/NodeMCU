e5a67295317ff873622e3cbe27a7d98c6e11284e
khthana <khthana@hotmail.com>
2017-05-14 08:29:10 +0700

    Create 2.0_8266_2.0_Input.ino

---
+++ 2.0_8266_2.0_Input.ino
@@ -0,0 +1,22 @@
+#define D8 10 // Mapping pin of NodeMCU with ESP8266
+#define button D8 // switch input Active Low
+#define pressed LOW
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(button,INPUT_PULLUP);
+}
+
+int i=0;
+void loop()
+{
+  bool ReadSwitch = digitalRead(button);
+  if(ReadSwitch == pressed)
+  {
+    Serial.print(i++);
+    Serial.println(" Pressed Switch.");
+    delay(500);
+  }
+}
+
